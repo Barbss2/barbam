6f12092f4b7e8d781527a36b7eb6311c8428d221
Barbss2 <53499777+Barbss2@users.noreply.github.com>
2019-07-31 09:43:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <html>
 <head><title>Hello</title></head>
 <br>
+<header>
 <h1>Hello World</h1>
+</header>
 <br>  
 <body>
+<main><h1 style="font-family: calibri".>This is the main></h1></main>
+<footer>
+  <p>Posted by: Charles Barba</p>
+  <p>Contact information: <a href="mailto:charlesbarbss@gmail.com">charlesbarbss@gmail.com</a>.</p>
+</footer>
 
+<p><strong>Note:</strong> Footer.</p>
 
+</body>
 </html>
